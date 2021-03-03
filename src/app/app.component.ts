270ee7ff39fd7dd82e1799ac255346aaa7230905
Pablo <36178185+Damnits@users.noreply.github.com>
2021-03-03 19:44:06 -0300

    Social-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/shared/model/usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'banco1';
+  title = 'Social IFPB';
+  usuario: Usuario;
+  usuarios: Array<Usuario>;
+  aluno= 'Pablo Fernandes Santos de Aragão Téjo';
+
+  constructor() {
+    this.usuario = new Usuario();
+    this.usuarios = new Array<Usuario>();
+  }
+  inserirUsuario(){
+    this.usuarios.push(this.usuario);
+    this.usuario = new Usuario();
+  }
 }
