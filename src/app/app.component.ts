bbb55253efa7eca7ec7878e0d0a7715f1d427f77
Pablo <36178185+Damnits@users.noreply.github.com>
2021-03-14 22:25:42 -0300

    intial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,13 @@ export class AppComponent {
   title = 'Social IFPB';
   usuario: Usuario;
   usuarios: Array<Usuario>;
-  aluno= 'Pablo Fernandes Santos de Aragão Téjo';
+  aluno = 'Pablo Fernandes Santos de Aragão Téjo';
 
   constructor() {
     this.usuario = new Usuario();
     this.usuarios = new Array<Usuario>();
   }
+
   inserirUsuario(){
     this.usuarios.push(this.usuario);
     this.usuario = new Usuario();
